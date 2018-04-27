2727bbf393bb2651806e2306809f3109a5615aaf
lucas <lucas.nandan@gmail.com>
2018-04-27 14:49:40 +0200

    ajout dossier et grid html

---
+++ js/script.js
@@ -0,0 +1,33 @@
+"use strict"
+
+class  Personnage {
+  constructor( nom, pv, dmg, power, ar, mr, ){
+    this.nom = nom;
+    this.pv = pv;
+    this.dmg = dmg;
+    this.power = power;
+    this.ar = ar;
+    this.mr = mr;
+  }
+  attaquer(cible){
+    let degat = this.dmg - cible.ar;
+    console.log(`${this.nom} à infligé ${degat} de dégats à ${cible.nom}`)
+    cible.pv = cible.pv - degat
+  }
+}
+
+class Sorcier extends Personnage {
+  constructor( nom, pv, dmg, power, ar, mr, ultime ){
+    super(nom, pv, dmg, power, ar, mr,)
+    this.ultime = ultime
+  }
+   utiliserUltime(cible){
+     this.pv<500{
+       this.dmg = this.ultime
+     }
+ }
+}
+const Kassadin = new Sorcier("Kassadin", 1500, 200, 300, 50, 200, 350)
+const Olaf = new Personnage("Olaf", 1500, 400, 0, 100, 50)
+
+Olaf.attaquer(Kassadin)
