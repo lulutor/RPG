c6f1229b5eb0f4f2f7805ee65501495bd006f723
lucas <lucas.nandan@gmail.com>
2018-04-30 16:58:17 +0200

    js class ok interraction can be tralalalala

--- js/script.js
+++ js/script.js
@@ -1,31 +1,53 @@
 "use strict"
 
 class  Personnage {
-  constructor( nom, pv, dmg, power, ar, mr, ){
-    this.nom = nom;
-    this.pv = pv;
-    this.dmg = dmg;
-    this.power = power;
-    this.ar = ar;
-    this.mr = mr;
+  constructor( nom, pv, force, magie, damage, armure, esquive, mana, ultime ){
+    this.nom = nom,
+    this.pv = pv,
+    this.force = force,
+    this.damage = damage,
+    this.armure = armure,
+    this.esquive = esquive,
+    this.mana = mana,
+    this.ultime= ultime
+  }
+}
+
+class Mage extends Personnage {
+  constructor( nom, pv, force, magie, damage, armure, esquive, mana, ultime ){
+    super(nom, pv, force, damage, armure, esquive, mana, ultime)
   }
   attaquer(cible){
-    let degat = this.dmg - cible.ar;
+    let degat = this.damage - cible.armure;
     console.log(`${this.nom} à infligé ${degat} de dégats à ${cible.nom}`)
     cible.pv = cible.pv - degat
+    document.querySelector("#pv_guerrier").textContent = Mordekaizer.pv;
   }
 }
 
-class Sorcier extends Personnage {
-  constructor( nom, pv, dmg, power, ar, mr, ultime ){
-    super(nom, pv, dmg, power, ar, mr,)
-    this.ultime = ultime
+class Guerrier extends Personnage {
+  constructor( nom, pv, force, magie, damage, armure, esquive, mana, ultime ){
+    super(nom, pv, force, damage, armure, esquive, mana, ultime)
   }
-   utiliserUltime(cible){
-     if(this.pv<500){ this.dmg = this.ultime }
- }
+  attaquer(cible){
+    let degat = this.damage - cible.armure;
+    console.log(`${this.nom} à infligé ${degat} de dégats à ${cible.nom}`)
+    cible.pv = cible.pv - degat
+    document.querySelector("#pv_mage").textContent = Syndra.pv;
+  }
 }
-const Kassadin = new Sorcier("Kassadin", 1500, 200, 300, 50, 200, 350)
-const Olaf = new Personnage("Olaf", 1500, 400, 0, 100, 50)
 
-Olaf.attaquer(Kassadin)
+const Syndra = new Mage("Syndra",1500, 50, 250, 50, 50, 0, 100, 400)
+const Mordekaizer = new Guerrier("Mordekaizer", 1500, 300, 50, 50, 120, 0, 0, 600)
+/*
+
+let item1 = document.querySelector("#item1")
+let item2 = document.querySelector("#item2")
+let item3 = document.querySelector("#item3")
+let item4= document.querySelector("#item4")
+let item5 = document.querySelector("#item5")
+let item6 = document.querySelector("#item6")
+
+item1.addEventListener("click" function() {
+  document.querySelector("body").style.backgroundColor = "yellow"
+})*/
