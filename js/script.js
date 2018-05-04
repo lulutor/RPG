1561720c757337a8cea5f7651984bdf265e2ff05
lucas <lucas.nandan@gmail.com>
2018-05-04 11:41:05 +0200

    test

--- js/script.js
+++ js/script.js
@@ -384,7 +384,7 @@ document.addEventListener('keypress', (event) => {
   if (event.key === "c") {
     var code = prompt('Entrer un code');
     if (code === "vincent") {
-      document.querySelector(".img_warriors").style.backgroundImage = "url('../images/strongman.png')"
+      document.querySelector(".img_warriors").style.backgroundImage = "url('../images/strongman2.jpeg')"
       document.querySelector("#strongman").textContent = "Strongman"
       Mordekaizer.moustache()
       Mordekaizer.afficherStatWarriors()
